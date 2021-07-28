ae0288f59a165fd3b9c4f3a227c6dfcaf3a07fd9
Ashlee <ashleeryan@outlook.com>
2021-07-28 11:35:07 +0100

    added nav styles

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import menu from '../../assets/images/menu-icon.png'
 import settings from '../../assets/images/settings-icon.png'
+import "./Nav.module.scss"
 import "../../App.scss"
 
 
 const Nav = () => {
     return (
-        <div>
+        <nav>
             <img src = {menu} />
             <h1>Ear Worm</h1>
             <img src = {settings} />   
-        </div>
+        </nav>
     )
 }
 
