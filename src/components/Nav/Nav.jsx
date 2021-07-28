295069d31a13bacea7acc576f5a0faaff5d4377d
Ashlee <ashleeryan@outlook.com>
2021-07-28 10:32:36 +0100

    Created component folder and Nav component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <span className = "menu"></span>
+            <h1>Ear Worm</h1>
+        </div>
+    )
+}
+
+export default Nav
