badce681310ac998bb16781c0ec7c863a449b185
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-08-05 17:19:26 +0100

    (feat): Created Carousel component

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+import "Carousel.scss";
+
+const Carousel = props => {
+  const { imagesArr } = props;
+  const [counter, setCounter] = useState(0);
+
+  return
+};
+
+export default Carousel;
