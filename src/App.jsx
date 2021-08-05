d1c24a3e62e4e0d120abd3a1dde77ade6c09487c
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-08-05 17:44:21 +0100

    (feat): Fleshing out the carousel, added images + basic functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.scss";
 import sunrise from "./assets/images/sunrise.png";
 import sun from "./assets/images/sun.png";
 import moon from "./assets/images/moon.png";
+
 import Nav from "./components/Nav/Nav";
 import Button from "./components/Button/Button";
 import DiscoverArtistCard from "./components/DiscoverArtistCard/DiscoverArtistCard";
 import DiscographyCardList from "./components/DiscographyCardList/DiscographyCardList";
+import Carousel from "./components/Carousel/Carousel";
 
 import albums from "./data/albums";
 import artist from "./data/artist";
@@ -30,14 +32,19 @@ const App = () => {
     greetingTime = "Evening!";
   }
 
-  // Filter
   const filteredAlbums = albums.filter(album => album.strAlbumThumb).slice(0, 9);
 
-  // Sort
   const highestRating = albums
     .filter(album => album.intScore)
     .sort((a, b) => b.intScore - a.intScore)
     .slice(0, 9);
+
+  const galleryImages = [
+    artist.strArtistFanart,
+    artist.strArtistFanart2,
+    artist.strArtistFanart3,
+    artist.strArtistFanart4,
+  ];
 
   return (
     <>
@@ -71,6 +78,11 @@ const App = () => {
           <div className="highest-rated">
             <DiscographyCardList title="Highest Rated" albumsArr={highestRating} />
           </div>
+        </section>
+
+        <section>
+          <h2>Gallery</h2>
+          <Carousel imagesArr={galleryImages} />
         </section>
       </div>
     </>
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
 
-import "Carousel.scss";
+import "./Carousel.scss";
+
+import leftArrow from "../../assets/images/left-arrow.png";
+import rightArrow from "../../assets/images/right-arrow.png";
 
 const Carousel = props => {
   const { imagesArr } = props;
   const [counter, setCounter] = useState(0);
 
-  return
+  const handleIncrement = () => {
+    if (counter < imagesArr.length - 1) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  return (
+    <div className="carousel">
+      <img src={leftArrow} alt="" onClick={handleDecrement} />
+      <img src={imagesArr[counter]} alt="" className="carousel__image" />
+      <img src={rightArrow} alt="" onClick={handleIncrement} />
+    </div>
+  );
 };
 
 export default Carousel;
